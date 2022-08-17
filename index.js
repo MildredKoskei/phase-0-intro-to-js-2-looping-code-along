de97cfdd5ed4a201f2bcd9d69da023e2af2eed9d
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-17 19:39:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards=["Guadalupe","Ollie","Aki"];
+const myArray=[]
+function writeCards(cards ,birthday){
+    for(let i =0;i<cards.length;i++){
+    myArray.push(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`);
+    }
+      return myArray;
+
+}
+function countDown(){
+    let counter = 10;
+    while(counter>=0){
+        console.log(counter--);
+    }
+}
